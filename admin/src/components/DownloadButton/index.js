51910d7e5face499f80f140541cdf74020ff332f
Zharkan <contact@gaylordjulien.dev>
2023-11-01 04:14:59 +0100

    add select

--- admin/src/components/DownloadButton/index.js
+++ admin/src/components/DownloadButton/index.js
@@ -1,4 +1,4 @@
-import { Button } from '@strapi/design-system';
+import { Button, Select, Option } from '@strapi/design-system';
 import { Download } from '@strapi/icons';
 import PropTypes from 'prop-types';
 import React, { memo } from 'react';
@@ -8,6 +8,7 @@ import { getTrad } from '../../utils';
 
 const DownloadButton = ({ urlToEncode, filename }) => {
   const { formatMessage } = useIntl();
+  const [extension, setExtension] = React.useState('svg');
 
   // get root url
   const rootUrl = window.location.origin;
@@ -22,24 +23,41 @@ const DownloadButton = ({ urlToEncode, filename }) => {
     window.open(
       `${rootUrl}/qrcode-generator?url=${encodeURIComponent(
         destination
-      )}&filename=${filename}&download=true`,
+      )}&filename=${filename}&ext=${extension}&download=true`,
       '_self'
     );
   };
 
+  console.log('extension', extension);
+
   return (
-    <Button
-      onClick={handleClick}
-      size="S"
-      startIcon={<Download />}
-      variant="secondary"
-      style={{ width: '100%' }}
-    >
-      {formatMessage({
-        id: getTrad('label.button'),
-        defaultMessage: 'Download my QR Code',
-      })}
-    </Button>
+    <>
+      <Select
+        label={'Select extension'}
+        placeholder={'Select extension'}
+        name="extension"
+        onChange={(e) => setExtension(e)}
+        value={extension}
+        onClear={() => setExtension('svg')}
+      >
+        <Option value="svg">SVG</Option>
+        <Option value="png">PNG</Option>
+        <Option value="jpeg">JPEG</Option>
+        <Option value="jpg">JPG</Option>
+      </Select>
+      <Button
+        onClick={handleClick}
+        size="S"
+        startIcon={<Download />}
+        variant="secondary"
+        style={{ width: '100%' }}
+      >
+        {formatMessage({
+          id: getTrad('label.button'),
+          defaultMessage: 'Download my QR Code',
+        })}
+      </Button>
+    </>
   );
 };
 
